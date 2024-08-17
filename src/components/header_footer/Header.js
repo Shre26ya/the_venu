fdc588c1395288adc7cd22d633249b84a3f9edef
Shre26ya <shreyasudhakaran5@gmail.com>
2024-08-17 18:13:47 -0400

    Header Updated

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { AppBar } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
     return (
-        <AppBar
-            position="fixed"
-            style={{
-                backgroundColor: '#2f2f2f',
-                boxShadow: 'none',
-                padding: '10px 0px'
+        <AppBar position="fixed" 
+        style={{ 
+            backgroundColor: '#2f2f2f',
+            boxShadow: 'none',
+            padding:'10px 0px' 
             }}
-        >
-            {/* AppBar content goes here */}
+            >
+            <Toolbar>
+                <div className= "header_logo">
+                    <div className="font_righteous header_logo_venue">The Venue</div>
+                    <div className="header_logo_title">Musical Events</div>
+                </div>
+            </Toolbar>
         </AppBar>
     );
 }
